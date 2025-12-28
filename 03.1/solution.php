1cee50e5940a3d52728887457859d65519373514
Matt Watson <matt@mattwatson.org>
2025-12-27 21:55:02 -0600

    Startee day 3

--- 03.1/solution.php
+++ 03.1/solution.php
@@ -1,11 +1,38 @@
-<style>
-    :root {
-        font-size: 32px;
-        font-family: monospace;
-    }
-</style>
 <?php
 
-$input_path = __DIR__ . '/real-input.txt';
+// not working at all
+
+$input_path = __DIR__ . '/test-input.txt';
 
 $input = file_get_contents($input_path);
+
+$banks = explode("\n", $input);
+
+$joltage_sets = '';
+
+$joltage_sum = 0;
+
+foreach($banks as $bank) {
+    $bs = array_reverse(str_split($bank));
+    $h = null;
+    $sh = null;
+    $hi = null;
+    $shi = null;
+    foreach($bs as $i => $b) {
+        if($b > $h) {
+            if($sh) {
+                $sh = $h;
+            }
+            $h = $b;
+            $hi = $i;
+        } else if($b > $sh) {
+            $sh = $b;
+            $shi = $i;
+        }
+    }
+    $joltage_sets .= "$h $sh<br>";
+    $joltage_sum += (int)($h.$sh);
+}
+
+echo $joltage_sets;
+echo $joltage_sum;
