38caa2785f1b63fa31f07b8c5c87398d07154f3c
Matt Watson <matt@mattwatson.org>
2025-12-31 15:08:14 -0600

    Works on test data, not on real data, ugh!!!!!

--- 06.2/solution.php
+++ 06.2/solution.php
@@ -1,59 +1,52 @@
 <?php
 
+// 6390681 too low
+// 25708293098210 too high
+// 5841154827 too low
+// 10184396490692 nor right
+
 $input_path = __DIR__ . '/test-input.txt';
 
 $input = file_get_contents($input_path);
 
 $lines = explode("\n", $input);
-$lines = array_map(fn($l) => explode(" ", $l), $lines);
-$lines = array_map(fn($l) => array_values(array_filter($l, fn($item) => $item !== '')), $lines);
+$rows = array_map(fn($l) => str_split($l), $lines);
+$num_cols = strlen(max(explode(" ", $input)));
+// echo $num_cols;
 
-$problems = [];
+$indexes = [];
 $sum = 0;
 
-foreach($lines as $lidx => $l) {
-    foreach($l as $idx => $n) {
-        if(!isset($problems[$idx])){
-            $problems[$idx] = [];
+foreach($rows as $row_i => $row) {
+    foreach($row as $col_i => $col) {
+        if(!isset($indexes[$col_i])) {
+            $indexes[$col_i] = [];
         }
-        if($lidx < count($lines) - 1) {
-            $problems[$idx][] = (int)$n;
-        } else {
-            $max_digits = strlen(((string)max($problems[$idx])));
-            var_dump($max_digits);
-            $problem = [];
-
-
-            foreach($problems[$idx] as $pn) {
-                $num = str_pad($pn, $max_digits, 'x', STR_PAD_RIGHT);
-                $digits = str_split($num);
-                var_dump($num);
-                foreach($digits as $didx => $digit) {
-                    // var_dump($digit, $didx);
-
-                    if($digit === 'x') {
-                        break;
-                    }
-                    if(!isset($problem[$didx])) {
-                        $problem[$didx] = '';
-                    }
-                    $problem[$didx] .= $digit;
-                }
-            }
-            // var_dump($problem);
-            $problem = array_map(fn($pr) => (int)$pr, $problem);
-            var_dump($problem);
-            if($n === '+') {
-                var_dump(array_sum($problem));
-                $sum += array_sum($problem);
-            } else {
-                var_dump(array_product($problem));
-                $sum += array_product($problem);
-            }
-        }
+        $indexes[$col_i][] = $col;
     }
 }
-// $pos = strpos($problems[0][2], '0');
-// var_dump($problems);
+// var_dump($indexes);
+foreach($indexes as $idx => $index) {
+    $operator = $index[array_key_last($index)];
+    array_pop($index);
+    // var_dump($index, $operator);
+    if($operator === ' ') continue;
+    $problem = [];
+
+    // var_dump($index);
+    for($count = $idx; $count < $idx + count($index); $count ++) {
+        // var_dump($count);
+        // if(isset($indexes[$count]))
+            $problem[] = implode('', $indexes[$count]);
+    }
+    // $problem = array_filter($problem, fn($item) => $item);
+    $problem = array_map(fn($item) => (int)$item, $problem);
+    // var_dump($problem);
+    if($operator === '+') {
+        $sum += array_sum($problem);
+    } else {
+        $sum += array_product($problem);
+    }
+}
 
 echo $sum;
